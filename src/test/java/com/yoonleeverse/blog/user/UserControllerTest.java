a09172f484ecf8a40c74bb39c681fd891055e6e1
dungbik <q1w2e30630@gmail.com>
2022-02-13 19:34:56 +0900

    Test: fix error

--- src/test/java/com/yoonleeverse/blog/user/UserControllerTest.java
+++ src/test/java/com/yoonleeverse/blog/user/UserControllerTest.java
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -30,7 +29,6 @@ import static java.lang.String.format;
 import static java.lang.Thread.*;
 import static org.assertj.core.api.Assertions.*;
 
-@DataJpaTest
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserControllerTest {
 

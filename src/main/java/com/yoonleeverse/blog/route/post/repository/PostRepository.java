172a32febdf80c798d977175f142f971231b9c05
dungbik <q1w2e30630@gmail.com>
2022-02-15 01:27:10 +0900

    Fix: default thumbnail image mistake

--- src/main/java/com/yoonleeverse/blog/route/post/repository/PostRepository.java
+++ src/main/java/com/yoonleeverse/blog/route/post/repository/PostRepository.java
@@ -14,15 +14,15 @@ import java.util.Optional;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    @Query("SELECT DISTINCT p FROM Post p JOIN FETCH p.postTags JOIN FETCH p.files JOIN FETCH p.thumbnail WHERE p.postId = (:id) ORDER BY p.createdDate DESC")
+    @Query("SELECT DISTINCT p FROM Post p JOIN FETCH p.postTags JOIN FETCH p.files LEFT JOIN FETCH p.thumbnail WHERE p.postId = (:id) ORDER BY p.createdDate DESC")
     Optional<Post> findById(@Param("id") Long id);
 
     @Query("SELECT p.postId FROM Post p ORDER BY p.createdDate DESC")
     Page<Long> getAllId(Pageable page);
 
-    @Query("SELECT DISTINCT p FROM Post p JOIN FETCH p.postTags JOIN FETCH p.files WHERE p.postId IN (:ids)")
+    @Query("SELECT DISTINCT p FROM Post p JOIN FETCH p.postTags JOIN FETCH p.files LEFT JOIN FETCH p.thumbnail WHERE p.postId IN (:ids)")
     List<Post> getAllByIds(@Param("ids") List<Long> ids);
 
-    @Query("SELECT DISTINCT p FROM Post p JOIN FETCH p.postTags JOIN FETCH p.files WHERE p.postId IN (:ids) ORDER BY p.createdDate DESC")
+    @Query("SELECT DISTINCT p FROM Post p JOIN FETCH p.postTags JOIN FETCH p.files LEFT JOIN FETCH p.thumbnail WHERE p.postId IN (:ids) ORDER BY p.createdDate DESC")
     List<Post> findAllById(@Param("ids") List<Long> ids);
 }

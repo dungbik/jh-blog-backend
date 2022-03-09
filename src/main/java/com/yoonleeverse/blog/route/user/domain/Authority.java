ee129ea452ec49876811785c5f7cf5fc1ecedc2c
dungbik <q1w2e30630@gmail.com>
2022-03-10 01:08:32 +0900

    Feat: add ssl

--- src/main/java/com/yoonleeverse/blog/route/user/domain/Authority.java
+++ src/main/java/com/yoonleeverse/blog/route/user/domain/Authority.java
@@ -2,7 +2,7 @@ package com.yoonleeverse.blog.route.user.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Authority implements GrantedAuthority {
-    ROLE_ADMIN, ROLE_USERß;
+    ROLE_ADMIN, ROLE_USER;
 
     public String getAuthority() {
         return name();
